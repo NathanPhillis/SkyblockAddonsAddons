715e43fb486ba439971f4467974326ef5648f2d9
NathanPhillis <nathan.phillis@yahoo.com.au>
2020-04-05 02:37:46 +0800

    Prepared for future new rarity and cleaned up

--- util/item.js
+++ util/item.js
@@ -1,39 +1,45 @@
 export default class Item {
 
-    static rarities = {
+    static rarity = {
         COMMON: 0,
         UNCOMMON: 1,
         RARE: 2,
         EPIC: 3,
         LEGENDARY: 5,
+        MYTHIC: 6,
         SPECIAL: 6
     };
 
     constructor(name, rarity) {
+        //name must be in camelCase
         this.name = formatItemText(name);
         this.rarity = rarity;
     }
 
     getDisplayName() {
         switch(this.rarity) {
-            case rarities.COMMON:
+            case rarity.COMMON:
                 return "&f" + this.name + "&f";
-            case rarities.UNCOMMON:
+            case rarity.UNCOMMON:
                 return "&a" + this.name + "&f";
-            case rarities.RARE:
+            case rarity.RARE:
                 return "&9" + this.name + "&f";
-            case rarities.EPIC:
+            case rarity.EPIC:
                 return "&5" + this.name + "&f";
-            case rarities.LEGENDARY:
+            case rarity.LEGENDARY:
                 return "&e" + this.name + "&f";
-            case rarities.SPECIAL:
+            //Future rarity
+            //case rarity.MYTHIC:
+            //    return "" + this.name + "&f";
+            case rarity.SPECIAL:
                 return "&d" + this.name + "&f";
             default:
                 return "&f" + this.name + "&f";
         }
     }
 
-    static formatItemText(str){
+    static formatItemName(str){
+        //RegEx witchcraft
         return str.match(/^[a-z]+|[A-Z][a-z]*/g).map(function(s){
             return s[0].toUpperCase() + s.substr(1).toLowerCase();
         }).join(' ');
